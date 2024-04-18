08b24d59c6eadac3c13396a85c7aa633ecb08afd
Rhythm Naik <rhythmstay4u@gmail.com>
2024-04-18 16:09:27 +0530

    first commit

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+      	<hr>
+
+      	<footer>
+        
+      	</footer>
+    </div> 
+
+
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
